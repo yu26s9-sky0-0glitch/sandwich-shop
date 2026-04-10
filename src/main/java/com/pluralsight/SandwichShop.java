0c7d396d892fb8ed83602fdc92cf3e1b18347e26
sahar omer <somer1@my.yearupunited.org>
2026-04-10 12:12:54 -0400

    added condation to successfully display the discounted price

---
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+import java.util.*;
+public class SandwichShop {
+    static void main(){
+    double sandwich1 = 5.45;
+    double sandwich2 = 8.95;
+    double kidsDiscount = 0.1;
+    double seniorDiscount = 0.2;
+    double calculatedDiscount = 0;
+    Scanner myScanner = new Scanner(System.in);
+    System.out.println("Choose the size: \n1: Regular: base price $5.45 \n2: Large: base price $8.95");
+    int sandwichSize = myScanner.nextInt();
+    System.out.println("What is your age: ");
+    int customerAge = myScanner.nextInt();
+    if (sandwichSize ==1){
+        if (customerAge < 18){
+            calculatedDiscount = sandwich1 - (sandwich1 * kidsDiscount);
+        }
+        else if (customerAge >= 65) {
+            calculatedDiscount = sandwich1 - (sandwich1*seniorDiscount);
+        }
+
+        }
+    else if (sandwichSize ==2){
+        if (customerAge < 18){
+            calculatedDiscount = sandwich2 - (sandwich2*kidsDiscount);
+        }
+        else if (customerAge >= 65){
+            calculatedDiscount = sandwich2 - (sandwich2*seniorDiscount);
+        }
+
+    }
+    System.out.println("Your sandwich will cost: " + calculatedDiscount);
+    }
+}
