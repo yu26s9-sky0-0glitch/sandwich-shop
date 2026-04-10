e5ab0e178b03a4952717639b8a05253c1d679368
sahar omer <somer1@my.yearupunited.org>
2026-04-10 15:06:55 -0400

    did exercise 2 in long format needs refactoring to get shorter and cleaner

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -11,27 +11,63 @@ public class SandwichShop {
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Choose the size: \n1: Regular: base price $5.45 \n2: Large: base price $8.95");
         int sandwichSize = myScanner.nextInt();
+        myScanner.nextLine();
+        System.out.println("Do want it loaded? y/n");
+        String isLoaded = myScanner.nextLine();
         System.out.println("What is your age: ");
         int customerAge = myScanner.nextInt();
-        if (sandwichSize == 1) {
+
+        if (sandwichSize == 1 && isLoaded.equals("y")){
+            sandwich1 += 1.00;
             if (customerAge < 18) {
                 calculatedDiscount = sandwich1 - (sandwich1 * kidsDiscount);
-            } else if (customerAge >= 65) {
+            }
+            else if (customerAge >= 65) {
                 calculatedDiscount = sandwich1 - (sandwich1 * seniorDiscount);
-            } else {
+            }
+            else {
                 calculatedDiscount = sandwich1;
             }
             validInput = true;
-        } else if (sandwichSize == 2) {
+        }
+        else if (sandwichSize == 1 && isLoaded.equals("n")){
+            if (customerAge < 18) {
+                calculatedDiscount = sandwich1 - (sandwich1 * kidsDiscount);
+            }
+            else if (customerAge >= 65) {
+                calculatedDiscount = sandwich1 - (sandwich1 * seniorDiscount);
+            }
+            else {
+                calculatedDiscount = sandwich1;
+            }
+            validInput = true;
+        }
+        else if (sandwichSize == 2 && isLoaded.equals("y")){
+            sandwich2 += 1.75;
             if (customerAge < 18) {
                 calculatedDiscount = sandwich2 - (sandwich2 * kidsDiscount);
-            } else if (customerAge >= 65) {
+            }
+            else if (customerAge >= 65) {
                 calculatedDiscount = sandwich2 - (sandwich2 * seniorDiscount);
-            } else {
+            }
+            else {
                 calculatedDiscount = sandwich2;
             }
             validInput = true;
-        } else {
+        }
+        else if (sandwichSize == 2  && isLoaded.equals("n")){
+            if (customerAge < 18) {
+                calculatedDiscount = sandwich2 - (sandwich2 * kidsDiscount);
+            }
+            else if (customerAge >= 65) {
+                calculatedDiscount = sandwich2 - (sandwich2 * seniorDiscount);
+            }
+            else {
+                calculatedDiscount = sandwich2;
+            }
+            validInput = true;
+        }
+        else {
             calculatedDiscount = 0.0;
         }
         if (validInput) {
